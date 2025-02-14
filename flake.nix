9626086eb792678f263057fcdc9d7e3629280ba4
bonsthie <barnabe.bonnet@gmail.com>
2025-02-14 18:10:13 +0100

    test flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "flake for the unstable version of Zig";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system: 
+      let 
+        pkgs = import nixpkgs { inherit system; };
+        zigLock = builtins.fromJSON (builtins.readFile ./zig-lock.json);
+        zigURL = zigLock.${system}.tarball;
+        zigSha256 = zigLock.${system}.shasum;
+
+        zig-unstable = pkgs.stdenv.mkDerivation {
+          pname = "zig-unstable";
+          version = "unstable";
+          src = pkgs.fetchTarball {
+            url = zigURL;
+            sha256 = zigSha256;
+          };
+          installPhase = ''
+            mkdir -p $out/bin
+            cp -r zig*/* $out/bin/
+          '';
+        };
+      in {
+        packages.default = zig-unstable;
+      }
+    );
+}
