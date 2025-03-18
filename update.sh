3e982e089f61f5de0fd6f823791943fabd5ff3bb
bonsthie <barnabe.bonnet@gmail.com>
2025-03-18 13:59:50 +0100

    fix script

--- update.sh
+++ update.sh
@@ -1,2 +1,11 @@
-export JSON_SHA256=$(nix-prefetch-url "https://ziglang.org/download/index.json" ) &&  nix-build compute-hash.nix --show-trace
+#!/usr/bin/env bash
+
+set -e  # Exit on error
+
+export JSON_SHA256=$(nix-prefetch-url "https://ziglang.org/download/index.json")
+
+FILE="$(nix-build compute-hash.nix --show-trace | tail -n 1)"
+echo "$FILE"
+cp "$FILE" "zig-lock.json"
+
 nix build
